9b5af7969af9d417fd280cc17937700e363bb607
Josiah_L <sjosiahleon@gmail.com>
2022-10-13 09:13:05 -0700

    began styling for menu

--- frontend/components/menu/menu.jsx
+++ frontend/components/menu/menu.jsx
@@ -5,19 +5,24 @@ import './menu.scss';
 
 const tabs = {
     splash: Splash
-}
+};
 
 
 const Menu = () => {
     const [tab, setTab] = useState('splash');
-
+    const changeTabTo = tab => e => {
+        if (tab in tabs) {
+            return setTab(tab)
+        }
+        console.error(`TabError: Tab ( ${tab} ) is not a tab!`)
+    };
 
     return (
         <div id='menu'>
             <div className="tab-container">{tabs[tab]()}</div>
             <div className='tab-selector'>
-                <button>welcome</button>
-                <button>graves</button>
+                <button onClick={changeTabTo('splash')} className={tab === 'splash' ? 'active' : ''}>welcome</button>
+                <button onClick={changeTabTo('graves')} className={tab === 'graves' ? 'active' : ''}>graves</button>
             </div>
         </div>
     )
